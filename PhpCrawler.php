9ee7c0a5d8763745f9b7d9938e38467f31fca0a9
Felipe Saruhashi <ffsaruhashi@gmail.com>
2014-08-17 10:53:54 -0300

    Depth First Function
    Implement the depth first function to 'walk' through the nodes.

--- PhpCrawler.php
+++ PhpCrawler.php
@@ -1,54 +1,80 @@
 <?php
 
-
 class phpCrawler {
 
-
-	function __construct() { }
-	//private __construct() { } 
-		/*
-		//check if the url it's valid
-		if ( filter_var($URL, FILTER_VALIDATE_URL) === TRUE ) {
-			$url = $URL;
-		}
-	
-		//check if depth it's an integer
-		if (is_int($DEPTH) ) {
-			$depth = $DEPTH;
-		}
-	} */
+	private $visitedURL;
+	private $count;
+	private $depth;
+	const max_nodes = 100;
 
 	public function getNodes($url) {
 
-		$htmlPage = file_get_contents($url);
-
+		$htmlPage = '';
+		if($url != '') { // if the url isn't empty
+			$htmlPage = file_get_contents($url);
+		}
 		$list = [];
 		$index = 0;
 
-
 		if ( $htmlPage != FALSE ) {
+			//if the html page was retrieved
 
 			//http://stackoverflow.com/questions/6090667/php-domdocument-errors-warnings-on-html5-tags
-			// libxml_use_internal_errors(true) and libxml_clear_errors() to hide html5 warnings
+			//libxml_use_internal_errors(true) and libxml_clear_errors() to hide html5 warnings
 			$dom = new DOMDocument;
 			libxml_use_internal_errors(true);
 			$dom->loadHTML($htmlPage);
 			libxml_clear_errors();
 
 			foreach( $dom->getElementsByTagName('a') as $node) {
-				$list[$index] = $node->getAttribute('href');
-				$index++;
+				if (!array_key_exists($node->getAttribute('href'),$this->visitedURL) && $this->count < 100) {
+					$list[$index] = $node->getAttribute('href');
+					$index++;
+				}
 			}
+
+			$this->visitedURL[$url] = TRUE;
 		}
 
 		return $list;
 	}
 
-	private $url;
-	private $depth;
+	public function run($URL, $DEPTH) {
 
-	private $htmlPage;
+		$this->visitedURL = []; //map with the already visited urls, to avoid cycles
+		$this->count = 0;
+		$this->depth = $DEPTH;
 
+		$this->visitedURL[$URL] = TRUE;//set as the root node as already visited.
+
+		$result[$URL] = $this->depthFirstSearch($URL, 1);//call the depth first search recursive function
+
+		return $result;
+	}
+
+
+	public function depthFirstSearch($url, $currentDepth) {
+		
+		if($this->count >= self::max_nodes || $currentDepth ==  $this->depth) {
+			//if already have
+			return [];
+		} else {
+			$return = [];
+
+			$queue = $this->getNodes($url);
+
+			foreach($queue as $nodes) {
+				if ($this->count >=self::max_nodes) {
+					break;
+				}
+				$this->count++; // increase the nodes counter
+				$return[$nodes] = $this->depthFirstSearch($url, $currentDepth + 1);				
+			}
+
+			return $return;
+		}
+
+	}
 }
 
 ?>
