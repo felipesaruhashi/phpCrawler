b1c1e968e8160e1f4ad16eada3b3f2de63fa4f12
Felipe Saruhashi <ffsaruhashi@gmail.com>
2014-08-17 15:53:35 -0300

    Index call
    Get the parameter's and call the crawler with.

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
-	require("phpCrawler.php");
+	require("PhpCrawler.php");
 
-	$
+	header('Content-Type: application/json');
+	$crawler = new PhpCrawler();
+
+
+	echo json_encode($crawler->run($_GET['url'], $_GET['depth']));
 
 	
 
